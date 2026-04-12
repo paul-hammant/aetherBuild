3e423f5064502c79499966ac5cb4d465c606ec7d
Paul Hammant <paul@hammant.org>
2026-04-12 14:46:09 +0100

    start of aeb taking over cargo.toml

--- itests/fetch-upstream.sh
+++ itests/fetch-upstream.sh
@@ -30,5 +30,6 @@ fetch_repo "https://github.com/fyne-io/fyne.git" "go-multimodule-fyne"
 
 fetch_repo "https://github.com/Oxen-AI/Oxen.git" "rust-multi-module-oxen"
 fetch_repo "https://github.com/SystemCraftsman/pants-python-monorepo-demo.git" "python-monorepo-demo"
+fetch_repo "https://github.com/mrhdias/store.git" "mrhdias_rust_store"
 
 echo "Done. Upstream sources restored."
